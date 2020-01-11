a891c734b23ef388880d2ef3968f0efc55b1ce9f
Asus <Asus@DESKTOP-7PSVCIM>
2020-01-12 01:17:28 +0600

    c++

---
+++ Setter and getter/src/Setter and getter.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Setter.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Friend.h"
+using namespace std;
+
+int main() {
+	Friend person;
+	cout << person.hisname() << endl;
+	return 0;
+}
